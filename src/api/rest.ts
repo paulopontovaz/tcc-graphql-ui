1eca4055651e34d865afe2391fa1bb89f27903c8
Paulo Vaz <paulopontovaz@gmail.com>
2021-11-17 22:46:21 +0100

    Finish first version of the migration to GraphQL

--- src/api/rest.ts
+++ src/api/rest.ts
@@ -1,18 +1,79 @@
-import axios from "axios";
+import { request, gql } from "graphql-request";
 import { Pokemon } from "../types/pokemon";
 
-const instance = axios.create({
-	baseURL: "http://localhost:3001/pokemon-api",
-});
+const API_URL = "http://localhost:3001/pokemon-api";
+
+const pokemonFindManyQuery = (query: string = "") => gql`
+	{
+		pokemonMany(filter: {nameRegex: "${query}"}, sort: _ID_ASC) {
+			id,
+			name,
+			type,
+			img
+		}
+	}
+`;
 
 export const fetchPokemonListService = (query: string = "") =>
-	instance.get(`/pokemon?q=${query}`);
+	request(API_URL, pokemonFindManyQuery(query)).then((res) => ({
+		data: res?.pokemonMany,
+	}));
+
+const pokemonFindOneQuery = (pokemonId: Pokemon["id"]) => gql`
+	{
+		pokemonOne(filter: {id: ${pokemonId}}) {
+			id,
+			name,
+			img,
+			nickname,
+			moves { 
+				level { 
+					learnedat, 
+					name 
+				} 
+			},
+			damages {
+				normal,
+				fire,
+				water,
+				electric,
+				grass,
+				ice,
+				fight,
+				poison,
+				ground,
+				flying,
+				psychic,
+				bug,
+				rock,
+				ghost,
+				dragon,
+				dark,
+				steel,
+			},
+			stats {
+				hp,
+				attack,
+				defense,
+				spattack,
+				spdefense,
+				speed,
+			},
+			misc {
+				abilities {
+					normal,
+					hidden
+				},
+				classification,
+				height,
+				weight,
+			},
+			type
+		}
+	}
+`;
 
 export const fetchPokemonService = (pokemonId: Pokemon["id"]) =>
-	instance.get(`/pokemon/${pokemonId}`);
-
-export const editPokemonService = (pokemon: Pokemon) =>
-	instance.put(`/pokemon/${pokemon.id}`, { nickname: pokemon.nickname });
-
-export const deletePokemonService = (pokemon: Pokemon) =>
-	instance.delete(`/pokemon/${pokemon.id}`);
+	request(API_URL, pokemonFindOneQuery(pokemonId)).then((res) => ({
+		data: res?.pokemonOne,
+	}));
